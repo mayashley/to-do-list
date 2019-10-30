61ac95fad14fd95c25e0e16272fd2ca2ed56b8f7
mayashley <54293844+mayashley@users.noreply.github.com>
2019-10-30 18:10:13 -0400

    Updates

---
+++ precache-manifest.144fe9322d87d33f1337ca98936d0ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09211f9bb274058f6549c9169e6a91c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "de07d63565ad26353323",
+    "url": "/to-do-list/static/css/main.b452d34c.chunk.css"
+  },
+  {
+    "revision": "6ae02b0ea27729ae6b5d",
+    "url": "/to-do-list/static/js/2.a83505ba.chunk.js"
+  },
+  {
+    "revision": "de07d63565ad26353323",
+    "url": "/to-do-list/static/js/main.2edab5e8.chunk.js"
+  },
+  {
+    "revision": "038f86d6469f081bf28c",
+    "url": "/to-do-list/static/js/runtime-main.ea8822b0.js"
+  },
+  {
+    "revision": "627562e8423b5c30aa4c66748d129145",
+    "url": "/to-do-list/static/media/Johnny.627562e8.jpg"
+  }
+]);
